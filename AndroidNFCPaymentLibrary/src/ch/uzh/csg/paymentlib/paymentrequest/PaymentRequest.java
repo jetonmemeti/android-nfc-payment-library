e463fdad0edc7647023684cb0b6c432ccecdb9c8
Jeton Memeti <jeton.memeti@gmail.com>
2014-05-16 23:59:32 +0200

    Refactored PaymentRequest.

--- AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/paymentrequest/PaymentRequest.java
+++ AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/paymentrequest/PaymentRequest.java
@@ -7,7 +7,6 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.security.SignatureException;
-import java.util.ArrayList;
 
 import ch.uzh.csg.nfclib.util.Utils;
 import ch.uzh.csg.paymentlib.exceptions.IllegalArgumentException;
@@ -15,58 +14,43 @@ import ch.uzh.csg.paymentlib.exceptions.NotSignedException;
 
 //TODO: javadoc
 public class PaymentRequest {
-	private byte version;
-	private byte signatureAlgorithm;
+	private int version;
+	private SignatureAlgorithm signatureAlgorithm;
 	
-	private byte usernameSellerLength;
-	private byte[] usernameSeller;
-	private byte usernameBuyerLength;
-	private byte[] usernameBuyer;
+	private String usernameSeller;
+	private String usernameBuyer;
 	
-	private byte currency;
-	private byte[] amount; //8 bytes (long)
-	private byte[] timestamp; //8 bytes (long)
-	private byte keyNumber;
+	private Currency currency;
+	private long amount;
+	private long timestamp;
+	private int keyNumber;
 	
-	private byte[] payload; //calculated from the fields above
+	/*
+	 * payload and signature are not serialized but only hold references in
+	 * order to save cpu time
+	 */
+	private byte[] payload;
 	private byte[] signature;
 	
+	private PaymentRequest() {
+	}
+	
+	//TODO: remove version from constructor? set always to 1?
 	public PaymentRequest(int version, SignatureAlgorithm signatureAlgorithm, String usernameSeller, String usernameBuyer, Currency currency, long amount, long timestamp, int keyNumber) throws IllegalArgumentException, UnsupportedEncodingException {
 		checkParameters(version, signatureAlgorithm, usernameSeller, usernameBuyer, currency, amount, timestamp, keyNumber);
 		
-		this.version = (byte) version;
-		this.signatureAlgorithm = signatureAlgorithm.getCode();
-		
-		this.usernameSeller = usernameSeller.getBytes("UTF-8");
-		this.usernameSellerLength = (byte) this.usernameSeller.length;
-		this.usernameBuyer = usernameBuyer.getBytes("UTF-8");
-		this.usernameBuyerLength = (byte) this.usernameBuyer.length;
-		
-		this.currency = currency.getCode();
-		this.amount = Utils.getLongAsBytes(amount);
-		this.timestamp = Utils.getLongAsBytes(timestamp);
-		this.keyNumber = (byte) keyNumber;
-		
-		setPayload();
-	}
-	
-	private PaymentRequest(byte version, byte signatureAlgorithm, byte usernameSellerLength, byte[] usernameSeller, byte usernameBuyerLength, byte[] usernameBuyer, byte currency, byte[] amount, byte[] timestamp, byte keyNumber) {
 		this.version = version;
 		this.signatureAlgorithm = signatureAlgorithm;
-		
-		this.usernameSellerLength = usernameSellerLength;
 		this.usernameSeller = usernameSeller;
-		this.usernameBuyerLength = usernameBuyerLength;
 		this.usernameBuyer = usernameBuyer;
-		
 		this.currency = currency;
-		this.amount = amount; //8 bytes (long)
-		this.timestamp = timestamp; //8 bytes (long)
+		this.amount = amount;
+		this.timestamp = timestamp;
 		this.keyNumber = keyNumber;
 		
 		setPayload();
 	}
-	
+
 	private void checkParameters(int version, SignatureAlgorithm signatureAlgorithm, String usernameSeller, String usernameBuyer, Currency currency, long amount, long timestamp, int keyNumber) throws IllegalArgumentException {
 		if (version <= 0 || version > 255)
 			throw new IllegalArgumentException("The version number must be between 0 and 255.");
@@ -93,42 +77,67 @@ public class PaymentRequest {
 			throw new IllegalArgumentException("The key number must be between 1 and 255.");
 	}
 	
-	private void setPayload() {
-		ArrayList<Byte> payload = new ArrayList<Byte>();
-		payload.add(version);
-		payload.add(signatureAlgorithm);
-		payload.add(usernameSellerLength);
-		for (int i=0; i<usernameSeller.length; i++) {
-			payload.add(usernameSeller[i]);
-		}
-		payload.add(usernameBuyerLength);
-		for (int i=0; i<usernameBuyer.length; i++) {
-			payload.add(usernameBuyer[i]);
-		}
-		payload.add(currency);
-		for (int i=0; i<amount.length; i++) {
-			payload.add(amount[i]);
-		}
-		for (int i=0; i<timestamp.length; i++) {
-			payload.add(timestamp[i]);
-		}
-		payload.add(keyNumber);
+	private void setPayload() throws UnsupportedEncodingException {
+		byte[] usernameSellerBytes = usernameSeller.getBytes("UTF-8");
+		byte[] usernameBuyerBytes = usernameBuyer.getBytes("UTF-8");
+		byte[] amountBytes = Utils.getLongAsBytes(amount);
+		byte[] timestampBytes = Utils.getLongAsBytes(timestamp);
 		
-		byte[] bytes = new byte[payload.size()];
-		for (int i=0; i<payload.size(); i++) {
-			bytes[i] = payload.get(i);
-		}
+		int length = 1+1+1+usernameSellerBytes.length+1+usernameBuyerBytes.length+1+8+8+1;
+		byte[] payload = new byte[length];
 		
-		this.payload = bytes;
+		int index = 0;
+		
+		payload[index++] = (byte) version;
+		payload[index++] = signatureAlgorithm.getCode();
+		payload[index++] = (byte) usernameSellerBytes.length;
+		for (byte b : usernameSellerBytes) {
+			payload[index++] = b;
+		}
+		payload[index++] = (byte) usernameBuyerBytes.length;
+		for (byte b : usernameBuyerBytes) {
+			payload[index++] = b;
+		}
+		payload[index++] = currency.getCode();
+		for (byte b : amountBytes) {
+			payload[index++] = b;
+		}
+		for (byte b : timestampBytes) {
+			payload[index++] = b;
+		}
+		payload[index++] = (byte) keyNumber;
+		
+		this.payload = payload;
 	}
-
-	public void sign(PrivateKey privateKey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-		Signature sig = Signature.getInstance(SignatureAlgorithm.getSignatureAlgorithm(signatureAlgorithm).getSignatureAlgorithm());
+	
+	protected byte[] getPayload() {
+		return payload;
+	}
+	
+	protected byte[] getSignature() throws NotSignedException {
+		if (signature == null)
+			throw new NotSignedException();
+		
+		return signature;
+	}
+	
+	public void sign(PrivateKey privateKey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, UnsupportedEncodingException {
+		Signature sig = Signature.getInstance(signatureAlgorithm.getSignatureAlgorithm());
 		sig.initSign(privateKey);
 		sig.update(payload);
 		signature = sig.sign();
 	}
-
+	
+	public boolean verify(PublicKey publicKey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, NotSignedException {
+		if (signature == null)
+			throw new NotSignedException();
+		
+		Signature sig = Signature.getInstance(signatureAlgorithm.getSignatureAlgorithm());
+		sig.initVerify(publicKey);
+		sig.update(this.payload);
+		return sig.verify(signature);
+	}
+	
 	/**
 	 * Returns the raw payload of this PaymentRequest and attaches the raw
 	 * signature to it.
@@ -153,7 +162,7 @@ public class PaymentRequest {
 		
 		return result;
 	}
-	
+
 	/**
 	 * Returns a PaymentRequest (payload and signature) based on the given
 	 * bytes.
@@ -166,61 +175,60 @@ public class PaymentRequest {
 	 */
 	public static PaymentRequest decode(byte[] bytes) throws IllegalArgumentException {
 		if (bytes == null)
-			throw new IllegalArgumentException("None of the arguments can be null.");
+			throw new IllegalArgumentException("The argument can't be null.");
 		
 		try {
 			int index = 0;
 			
-			byte version = bytes[index++];
-			byte signatureAlgorithm = bytes[index++];
+			PaymentRequest pr = new PaymentRequest();
+			pr.version = bytes[index++] & 0xFF;
+			pr.signatureAlgorithm = SignatureAlgorithm.getSignatureAlgorithm(bytes[index++]);
 			
-			byte usernameSellerLength = bytes[index++];
-			int usLength = usernameSellerLength & 0xFF;
-			byte[] usernameSeller = new byte[usLength];
-			for (int i=0; i<usLength; i++) {
-				usernameSeller[i] = bytes[index++];
+			int usernameSellerLength = bytes[index++] & 0xFF;
+			byte[] usernameSellerBytes = new byte[usernameSellerLength];
+			for (int i=0; i<usernameSellerLength; i++) {
+				usernameSellerBytes[i] = bytes[index++];
 			}
+			pr.usernameSeller = new String(usernameSellerBytes);
 			
-			byte usernameBuyerLength = bytes[index++];
-			int ubLength = usernameBuyerLength & 0xFF;
-			byte[] usernameBuyer = new byte[ubLength];
-			for (int i=0; i<ubLength; i++) {
-				usernameBuyer[i] = bytes[index++];
+			int usernameBuyerLength = bytes[index++] & 0xFF;
+			byte[] usernameBuyerBytes = new byte[usernameBuyerLength];
+			for (int i=0; i<usernameBuyerLength; i++) {
+				usernameBuyerBytes[i] = bytes[index++];
 			}
+			pr.usernameBuyer = new String(usernameBuyerBytes);
 			
-			byte currency = bytes[index++];
+			pr.currency = Currency.getCurrency(bytes[index++]);
 			
-			byte[] amount = new byte[Long.SIZE / Byte.SIZE]; //8 bytes (long)
-			for (int i=0; i<amount.length; i++) {
-				amount[i] = bytes[index++];
+			byte[] amountBytes = new byte[Long.SIZE / Byte.SIZE]; //8 bytes (long)
+			for (int i=0; i<amountBytes.length; i++) {
+				amountBytes[i] = bytes[index++];
 			}
+			pr.amount = Utils.getBytesAsLong(amountBytes);
 			
-			byte[] timestamp = new byte[Long.SIZE / Byte.SIZE]; //8 bytes (long)
-			for (int i=0; i<timestamp.length; i++) {
-				timestamp[i] = bytes[index++];
+			byte[] timestampBytes = new byte[Long.SIZE / Byte.SIZE]; //8 bytes (long)
+			for (int i=0; i<timestampBytes.length; i++) {
+				timestampBytes[i] = bytes[index++];
 			}
+			pr.timestamp = Utils.getBytesAsLong(timestampBytes);
 			
-			byte keyNumber = bytes[index++];
+			pr.keyNumber = bytes[index++] & 0xFF;
+			
+			pr.setPayload();
 			
 			byte[] signature = new byte[bytes.length - index];
 			for (int i=0; i<signature.length; i++) {
 				signature[i] = bytes[index++];
 			}
 			
-			PaymentRequest pr = new PaymentRequest(version, signatureAlgorithm, usernameSellerLength, usernameSeller, usernameBuyerLength, usernameBuyer, currency, amount, timestamp, keyNumber);
 			pr.signature = signature;
 			
 			return pr;
 		} catch (IndexOutOfBoundsException e) {
 			throw new IllegalArgumentException("The given byte array is corrupt (not long enough).");
+		} catch (UnsupportedEncodingException e) {
+			throw new IllegalArgumentException("The given byte array is corrupt.");
 		}
-	}
-	
-	public boolean verify(PublicKey publicKey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-		Signature sig = Signature.getInstance(SignatureAlgorithm.getSignatureAlgorithm(signatureAlgorithm).getSignatureAlgorithm());
-		sig.initVerify(publicKey);
-		sig.update(this.payload);
-		return sig.verify(signature);
 	}
 	
 }
