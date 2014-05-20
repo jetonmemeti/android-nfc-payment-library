06793a2ad5f30a03706eb1d6d1a05789539f315c
Jeton Memeti <jeton.memeti@gmail.com>
2014-05-20 18:06:44 +0200

    Refactored UserInfos.

--- AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/container/UserInfos.java
+++ AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/container/UserInfos.java
@@ -7,36 +7,33 @@ import ch.uzh.csg.paymentlib.exceptions.IllegalArgumentException;
 import ch.uzh.csg.paymentlib.paymentrequest.SignatureAlgorithm;
 
 //TODO: javadoc
-public class PayerInfos {
+public class UserInfos {
 	
 	private String username;
 	private PrivateKey privateKey;
-	private String keyAlgorithm;
+	private SignatureAlgorithm signatureAlgorithm;
 	private int keyNumber;
 	private long userId;
 	
-	public PayerInfos(String username, PrivateKey privateKey, String keyAlgorithm, int keyNumber) throws IllegalArgumentException {
-		checkParams(username, privateKey, keyAlgorithm, keyNumber);
+	public UserInfos(String username, PrivateKey privateKey, SignatureAlgorithm signatureAlgorithm, int keyNumber) throws IllegalArgumentException {
+		checkParams(username, privateKey, signatureAlgorithm, keyNumber);
 		
 		this.username = username;
 		this.privateKey = privateKey;
-		this.keyAlgorithm = keyAlgorithm;
+		this.signatureAlgorithm = signatureAlgorithm;
 		this.keyNumber = keyNumber;
 		this.userId = new Random().nextLong();
 	}
 	
-	private void checkParams(String username, PrivateKey privateKey, String keyAlgorithm, int keyNumber) throws IllegalArgumentException {
+	private void checkParams(String username, PrivateKey privateKey, SignatureAlgorithm signatureAlgorithm, int keyNumber) throws IllegalArgumentException {
 		if (username == null || username.isEmpty() || username.length() > 255)
 			throw new IllegalArgumentException("The username cannot be null, empty, or longer than 255 characters.");
 		
 		if (privateKey == null)
 			throw new IllegalArgumentException("The privatekey cannot be null.");
 			
-		if (keyAlgorithm == null || keyAlgorithm.isEmpty())
-			throw new IllegalArgumentException("The key algorithm cannot be null or empty.");
-		
-		if (!SignatureAlgorithm.isKeyGenAlgorithmSupported(keyAlgorithm))
-			throw new IllegalArgumentException("The key algorithm "+keyAlgorithm+" is not supported.");
+		if (signatureAlgorithm == null)
+			throw new IllegalArgumentException("The signature algorithm cannot be null or empty.");
 		
 		if (keyNumber <= 0 || keyNumber > 255)
 			throw new IllegalArgumentException("The key number must be between 1 and 255.");
@@ -50,8 +47,8 @@ public class PayerInfos {
 		return privateKey;
 	}
 
-	public String getKeyAlgorithm() {
-		return keyAlgorithm;
+	public SignatureAlgorithm getSignatureAlgorithm() {
+		return signatureAlgorithm;
 	}
 
 	public int getKeyNumber() {
