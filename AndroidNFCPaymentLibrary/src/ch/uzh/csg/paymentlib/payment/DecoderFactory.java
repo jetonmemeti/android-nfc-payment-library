95be6101620b212050894c3d8b028450528c9494
Jeton Memeti <jeton.memeti@gmail.com>
2014-05-22 15:08:40 +0200

    Added DecoderFactory for deserializing objects.

---
+++ AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/payment/DecoderFactory.java
@@ -0,0 +1,22 @@
+package ch.uzh.csg.paymentlib.payment;
+
+import ch.uzh.csg.paymentlib.exceptions.IllegalArgumentException;
+import ch.uzh.csg.paymentlib.exceptions.NotSignedException;
+import ch.uzh.csg.paymentlib.exceptions.UnknownCurrencyException;
+import ch.uzh.csg.paymentlib.exceptions.UnknownSignatureAlgorithmException;
+
+//TODO: javadoc
+public class DecoderFactory {
+	
+	@SuppressWarnings("unchecked")
+	public static <T extends SerializableObject> T decode(Class<? extends SerializableObject> clazz, byte[] bytes, int x) throws IllegalAccessException, IllegalArgumentException, NotSignedException, UnknownSignatureAlgorithmException, UnknownCurrencyException {
+		try {
+			T t = (T) clazz.newInstance();
+			t.decode(bytes);
+			return t;
+		} catch (InstantiationException e) {
+			return null;
+		}
+	}
+	
+}
