6c3bb4f1ffe1789062a978dd51efe3b5c00b876a
Jeton Memeti <jeton.memeti@gmail.com>
2014-05-23 14:17:42 +0200

    Refactored ServerPaymentResponse.

--- AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/payment/ServerPaymentResponse.java
+++ AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/payment/ServerPaymentResponse.java
@@ -1,7 +1,5 @@
 package ch.uzh.csg.paymentlib.payment;
 
-import java.io.UnsupportedEncodingException;
-
 import ch.uzh.csg.nfclib.util.Utils;
 import ch.uzh.csg.paymentlib.exceptions.IllegalArgumentException;
 import ch.uzh.csg.paymentlib.exceptions.NotSignedException;
@@ -9,108 +7,123 @@ import ch.uzh.csg.paymentlib.exceptions.UnknownCurrencyException;
 import ch.uzh.csg.paymentlib.exceptions.UnknownSignatureAlgorithmException;
 
 //TODO: javadoc
-public class ServerPaymentResponse extends SignedSerializableObject {
-	private static final int NOF_BYTES_FOR_PAYMENT_REQUEST_LENGTH = 2; // 2 bytes for the payload length, up to 65536 bytes
-
-	private PaymentRequest paymentRequest;
-	private ServerResponseStatus status;
-	private String reason;
+public class ServerPaymentResponse extends SerializableObject {
+	private static final int NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH = 2; // 2 bytes for the payload length, up to 65536 bytes
+	
+	private byte nofPaymentResponses;
+	
+	private PaymentResponse paymentResponsePayer;
+	private PaymentResponse paymentResponsePayee;
 	
 	//this constructor is needed for the DecoderFactory
 	protected ServerPaymentResponse() {
 	}
-	
-	public ServerPaymentResponse(SignatureAlgorithm signatureAlgorithm, PaymentRequest paymentRequest, ServerResponseStatus status, String reason) throws IllegalArgumentException, UnsupportedEncodingException {
-		this(1, signatureAlgorithm, paymentRequest, status, reason);
+
+	public ServerPaymentResponse(PaymentResponse paymentResponsePayer) throws IllegalArgumentException {
+		this(1, paymentResponsePayer);
 	}
 	
-	private ServerPaymentResponse(int version, SignatureAlgorithm signatureAlgorithm, PaymentRequest paymentRequest, ServerResponseStatus status, String reason) throws IllegalArgumentException, UnsupportedEncodingException {
-		super(1, signatureAlgorithm);
-		checkParameters(paymentRequest, status, reason);
-		
-		this.paymentRequest = paymentRequest;
-		this.status = status;
-		this.reason = reason;
-		
-		setPayload();
+	public ServerPaymentResponse(PaymentResponse paymentResponsePayer, PaymentResponse paymentResponsePayee) throws IllegalArgumentException {
+		this(1, paymentResponsePayer, paymentResponsePayee);
 	}
 	
-	private static void checkParameters(PaymentRequest paymentRequest, ServerResponseStatus status, String reason) throws IllegalArgumentException {
-		if (paymentRequest == null)
-			throw new IllegalArgumentException("The payment request cannot be null.");
+	private ServerPaymentResponse(int version, PaymentResponse paymentResponsePayer) throws IllegalArgumentException {
+		super(version);
+		checkParameters(paymentResponsePayer, "payer");
+		this.paymentResponsePayer = paymentResponsePayer;
+		this.nofPaymentResponses = 1;
+	}
+	
+	private ServerPaymentResponse(int version, PaymentResponse paymentResponsePayer, PaymentResponse paymentResponsePayee) throws IllegalArgumentException {
+		super(version);
 		
-		int maxPayloadLength = (int) Math.pow(2, NOF_BYTES_FOR_PAYMENT_REQUEST_LENGTH*Byte.SIZE) - 1;
-		byte[] payload = paymentRequest.getPayload();
+		checkParameters(paymentResponsePayer, "payer");
+		checkParameters(paymentResponsePayee, "payee");
+		
+		this.paymentResponsePayer = paymentResponsePayer;
+		this.paymentResponsePayee = paymentResponsePayee;
+		this.nofPaymentResponses = 2;
+	}
+	
+	private static void checkParameters(PaymentResponse paymentResponse, String role) throws IllegalArgumentException {
+		if (paymentResponse == null)
+			throw new IllegalArgumentException("The payment response cannot be null.");
+		
+		int maxPayloadLength = (int) Math.pow(2, NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH*Byte.SIZE) - 1;
+		byte[] payload = paymentResponse.getPayload();
 		if (payload == null || payload.length == 0 || payload.length > maxPayloadLength)
-			throw new IllegalArgumentException("The payment requests's payload can't be null, empty or longer than "+maxPayloadLength+" bytes.");
+			throw new IllegalArgumentException("The "+role+"'s payment response payload can't be null, empty or longer than "+maxPayloadLength+" bytes.");
 		
-		if (status == null)
-			throw new IllegalArgumentException("The status cannot be null.");
+		byte[] signature = paymentResponse.getSignature();
+		if (signature == null || signature.length == 0)
+			throw new IllegalArgumentException("The "+role+"'s payment response is not signed.");
 		
-		if (status.getCode() == 2) {
-			if (reason == null)
-				throw new IllegalArgumentException("The reason cannot be null if the status is set to FAILURE.");
-			if (reason.length() > 255)
-				throw new IllegalArgumentException("The reason cannot be longer than 255 characters.");
-		}
+		if (signature.length > 255)
+			throw new IllegalArgumentException("The "+role+"'s payment response signature is too long. A signature algorithm with output longer than 255 bytes is not supported.");
 	}
 	
-	private void setPayload() throws UnsupportedEncodingException {
-		/*
-		 * version
-		 * + signatureAlgorithm
-		 * + paymentRequest.length
-		 * + paymentRequest
-		 * + status
-		 * + reason.length
-		 * + reason
-		 */
-		byte[] reasonBytes = null;
-		int length;
-		if (status == ServerResponseStatus.SUCCESS) {
-			length = 1+1+NOF_BYTES_FOR_PAYMENT_REQUEST_LENGTH+paymentRequest.getPayload().length+1;
-		} else {
-			reasonBytes = reason.getBytes("UTF-8");
-			length = 1+1+NOF_BYTES_FOR_PAYMENT_REQUEST_LENGTH+paymentRequest.getPayload().length+1+1+reasonBytes.length;
-		}
+	public PaymentResponse getPaymentResponsePayer() {
+		return paymentResponsePayer;
+	}
+
+	public PaymentResponse getPaymentResponsePayee() {
+		return paymentResponsePayee;
+	}
+
+	@Override
+	public byte[] encode() throws NotSignedException {
+		byte[] paymentResponsePayerRaw = null;
+		byte[] paymentResponsePayeeRaw = null;
 		
-		byte[] payload = new byte[length];
+		int length;
+		if (nofPaymentResponses == 1) {
+			paymentResponsePayerRaw = paymentResponsePayer.encode();
+			/*
+			 * version
+			 * + nofPaymentResponses
+			 * + paymentResponsePayer.length
+			 * + paymentResponsePayer
+			 */
+			length = 1+1+NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH+paymentResponsePayerRaw.length;
+		} else {
+			paymentResponsePayerRaw = paymentResponsePayer.encode();
+			paymentResponsePayeeRaw = paymentResponsePayee.encode();
+			/*
+			 * version
+			 * + nofPaymentResponses
+			 * + paymentResponsePayer.length
+			 * + paymentResponsePayer
+			 * + paymentResponsePayee.length
+			 * + paymentResponsePayee
+			 */
+			length = 1+1+NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH+paymentResponsePayerRaw.length+NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH+paymentResponsePayeeRaw.length;
+		}
 		
 		int index = 0;
+		byte[] result = new byte[length];
 		
-		payload[index++] = (byte) getVersion();
-		payload[index++] = getSignatureAlgorithm().getCode();
+		result[index++] = (byte) getVersion();
+		result[index++] = nofPaymentResponses;
 		
-		byte[] paymentRequestLengthBytes = Utils.getShortAsBytes((short) paymentRequest.getPayload().length);
-		for (byte b : paymentRequestLengthBytes) {
-			payload[index++] = b;
+		byte[] paymentResponsePayerLengthBytes = Utils.getShortAsBytes((short) paymentResponsePayerRaw.length);
+		for (byte b : paymentResponsePayerLengthBytes) {
+			result[index++] = b;
 		}
-		for (byte b : paymentRequest.getPayload()) {
-			payload[index++] = b;
+		for (byte b : paymentResponsePayerRaw) {
+			result[index++] = b;
 		}
 		
-		payload[index++] = status.getCode();
-		
-		if (status == ServerResponseStatus.FAILURE) {
-			payload[index++] = (byte) reason.length();
-			for (byte b : reasonBytes) {
-				payload[index++] = b;
+		if (nofPaymentResponses > 1) {
+			byte[] paymentResponsePayeeLengthBytes = Utils.getShortAsBytes((short) paymentResponsePayeeRaw.length);
+			for (byte b : paymentResponsePayeeLengthBytes) {
+				result[index++] = b;
+			}
+			for (byte b : paymentResponsePayeeRaw) {
+				result[index++] = b;
 			}
 		}
 		
-		this.payload = payload;
-	}
-	
-	public PaymentRequest getPaymentRequest() {
-		return paymentRequest;
-	}
-	
-	public ServerResponseStatus getStatus() {
-		return status;
-	}
-	
-	public String getReason() {
-		return reason;
+		return result;
 	}
 	
 	@Override
@@ -121,53 +134,42 @@ public class ServerPaymentResponse extends SignedSerializableObject {
 		try {
 			int index = 0;
 			
-			int version = bytes[index++];
-			SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.getSignatureAlgorithm(bytes[index++]);
+			int version = (bytes[index++] & 0xFF);
+			byte nofPaymentResponses = bytes[index++];
 			
-			byte[] paymentRequestLengthBytes = new byte[NOF_BYTES_FOR_PAYMENT_REQUEST_LENGTH];
-			for (int i=0; i<NOF_BYTES_FOR_PAYMENT_REQUEST_LENGTH; i++) {
-				paymentRequestLengthBytes[i] = bytes[index++];
+			byte[] indicatedPaymentResponsePayerLengthBytes = new byte[NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH];
+			for (int i=0; i<NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH; i++) {
+				indicatedPaymentResponsePayerLengthBytes[i] = bytes[index++];
 			}
-			int paymentRequestLength = Utils.getBytesAsShort(paymentRequestLengthBytes) & 0xFF;
 			
-			byte[] paymentRequestBytes = new byte[paymentRequestLength];
-			for (int i=0; i<paymentRequestLength; i++) {
-				paymentRequestBytes[i] = bytes[index++];
+			int paymentResponsePayerLength = (Utils.getBytesAsShort(indicatedPaymentResponsePayerLengthBytes) & 0xFF);
+			byte[] paymentResponsePayerBytes = new byte[paymentResponsePayerLength];
+			for (int i=0; i<paymentResponsePayerLength; i++) {
+				paymentResponsePayerBytes[i] = bytes[index++];
 			}
-			PaymentRequest paymentRequest = DecoderFactory.decode(PaymentRequest.class, paymentRequestBytes);
-
-			ServerResponseStatus status = ServerResponseStatus.getStatus(bytes[index++]);
 			
-			String reason;
-			if (status == ServerResponseStatus.FAILURE) {
-				int reasonLength = bytes[index++] & 0xFF;
-				byte[] reasonBytes = new byte[reasonLength];
-				for (int i=0; i<reasonLength; i++) {
-					reasonBytes[i] = bytes[index++];
+			PaymentResponse paymentResponsePayer = DecoderFactory.decode(PaymentResponse.class, paymentResponsePayerBytes);
+			if (nofPaymentResponses == 1) {
+				return new ServerPaymentResponse(version, paymentResponsePayer);
+			} else if (nofPaymentResponses == 2) {
+				byte[] indicatedPaymentResponsePayeeLengthBytes = new byte[NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH];
+				for (int i=0; i<NOF_BYTES_FOR_PAYMENT_RESPONSE_LENGTH; i++) {
+					indicatedPaymentResponsePayeeLengthBytes[i] = bytes[index++];
 				}
-				reason = new String(reasonBytes);
-			} else {
-				reason = null;
-			}
-			
-			ServerPaymentResponse sr = new ServerPaymentResponse(version, signatureAlgorithm, paymentRequest, status, reason);
-			
-			int signatureLength = bytes.length - index;
-			if (signatureLength == 0) {
-				throw new NotSignedException();
-			} else {
-				byte[] signature = new byte[signatureLength];
-				for (int i=0; i<signature.length; i++) {
-					signature[i] = bytes[index++];
+				
+				int paymentResponsePayeeLength = (Utils.getBytesAsShort(indicatedPaymentResponsePayeeLengthBytes) & 0xFF);
+				byte[] paymentResponsePayeeBytes = new byte[paymentResponsePayeeLength];
+				for (int i=0; i<paymentResponsePayeeLength; i++) {
+					paymentResponsePayeeBytes[i] = bytes[index++];
 				}
-				sr.signature = signature;
+				
+				PaymentResponse paymentResponsePayee = DecoderFactory.decode(PaymentResponse.class, paymentResponsePayeeBytes);
+				return new ServerPaymentResponse(version, paymentResponsePayer, paymentResponsePayee);
+			} else {
+				throw new IllegalArgumentException("The given byte array is corrupt.");
 			}
-			
-			return sr;
 		} catch (IndexOutOfBoundsException e) {
 			throw new IllegalArgumentException("The given byte array is corrupt (not long enough).");
-		} catch (UnsupportedEncodingException e) {
-			throw new IllegalArgumentException("The given byte array is corrupt.");
 		}
 	}
 	
@@ -178,23 +180,17 @@ public class ServerPaymentResponse extends SignedSerializableObject {
 		if (!(o instanceof ServerPaymentResponse))
 			return false;
 		
-		ServerPaymentResponse sr = (ServerPaymentResponse) o;
-		if (getVersion() != sr.getVersion())
+		ServerPaymentResponse spr = (ServerPaymentResponse) o;
+		if (getVersion() != spr.getVersion())
 			return false;
-		if (getSignatureAlgorithm().getCode() != sr.getSignatureAlgorithm().getCode())
+		if (this.nofPaymentResponses != spr.nofPaymentResponses)
 			return false;
-		if (!this.paymentRequest.equals(sr.paymentRequest))
+		if (!getPaymentResponsePayer().equals(spr.getPaymentResponsePayer()))
 			return false;
-		if (this.status.getCode() != sr.status.getCode())
-			return false;
-		if (this.reason == null && sr.reason != null)
-			return false;
-		if (this.reason != null && sr.reason == null)
-			return false;
-		if (this.reason == null && sr.reason == null)
-			return true;
-		if (!this.reason.equals(sr.reason))
-			return false;
+		if (nofPaymentResponses == 2) {
+			if (!getPaymentResponsePayer().equals(spr.getPaymentResponsePayer()))
+				return false;
+		}
 		
 		return true;
 	}
