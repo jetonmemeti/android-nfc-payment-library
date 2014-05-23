a3d375ebc2ec4d330256bfa67ef55a0dae017054
Jeton Memeti <jeton.memeti@gmail.com>
2014-05-23 16:42:53 +0200

    Refactored ServerInfos.

--- AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/container/ServerInfos.java
+++ AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/container/ServerInfos.java
@@ -3,35 +3,21 @@ package ch.uzh.csg.paymentlib.container;
 import java.security.PublicKey;
 
 import ch.uzh.csg.paymentlib.exceptions.IllegalArgumentException;
-import ch.uzh.csg.paymentlib.payment.SignatureAlgorithm;
 
 //TODO: javadoc
 public class ServerInfos {
 	
 	private PublicKey publicKey;
-	private SignatureAlgorithm signatureAlgorithm;
 	
-	public ServerInfos(PublicKey publicKey, SignatureAlgorithm signatureAlgorithm) throws IllegalArgumentException {
-		checkParameters(publicKey, signatureAlgorithm);
-		
-		this.publicKey = publicKey;
-		this.signatureAlgorithm = signatureAlgorithm;
-	}
-
-	private void checkParameters(PublicKey publicKey, SignatureAlgorithm signatureAlgorithm) throws IllegalArgumentException {
+	public ServerInfos(PublicKey publicKey) throws IllegalArgumentException {
 		if (publicKey == null)
 			throw new IllegalArgumentException("The privatekey cannot be null.");
-			
-		if (signatureAlgorithm == null)
-			throw new IllegalArgumentException("The signature algorithm cannot be null or empty.");
+		
+		this.publicKey = publicKey;
 	}
 
 	public PublicKey getPublicKey() {
 		return publicKey;
 	}
 
-	public SignatureAlgorithm getSignatureAlgorithm() {
-		return signatureAlgorithm;
-	}
-	
 }
