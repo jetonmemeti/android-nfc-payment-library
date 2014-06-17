dcd7081a7b543b97e2096864cce3ffffc586b687
Jeton Memeti <jeton.memeti@gmail.com>
2014-06-17 15:52:03 +0200

    Fixed TODO.

--- AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/util/Config.java
+++ AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/util/Config.java
@@ -3,8 +3,7 @@ package ch.uzh.csg.paymentlib.util;
 //TODO: javadoc
 public class Config {
 	
-	//TODO: is there a difference between these two?
-	public static final long SERVER_CALL_TIMEOUT = 1500; //in ms // server call
-	public static final long SERVER_RESPONSE_TIMEOUT = 5000; //in ms //paymentrequesthandler waiting for server response
+	public static final long SERVER_CALL_TIMEOUT = 3 * 1000; //in ms - server call
+	public static final long SERVER_RESPONSE_TIMEOUT = 4 * 000; //in ms - PaymentRequestHandler waiting for server response
 
 }
