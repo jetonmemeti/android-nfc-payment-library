c23cba608e0a109314c53ed78cb2273bda32559a
Jeton Memeti <jeton.memeti@gmail.com>
2014-06-23 14:12:28 +0200

    Fixed typo in config.

--- AndroidNFCPaymentLibrary/src/main/java/ch/uzh/csg/paymentlib/util/Config.java
+++ AndroidNFCPaymentLibrary/src/main/java/ch/uzh/csg/paymentlib/util/Config.java
@@ -4,6 +4,6 @@ package ch.uzh.csg.paymentlib.util;
 public class Config {
 	
 	public static final long SERVER_CALL_TIMEOUT = 3 * 1000; //in ms - server call
-	public static final long SERVER_RESPONSE_TIMEOUT = 4 * 000; //in ms - PaymentRequestHandler waiting for server response
+	public static final long SERVER_RESPONSE_TIMEOUT = 4 * 1000; //in ms - PaymentRequestHandler waiting for server response
 
 }
