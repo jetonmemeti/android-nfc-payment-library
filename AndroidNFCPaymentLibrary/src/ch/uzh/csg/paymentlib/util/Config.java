f7d7bbe8178d545269628228f277314b9954bb52
Jeton Memeti <jeton.memeti@gmail.com>
2014-06-24 21:23:18 +0200

    Removed not used resources (not deleted in merge).

--- AndroidNFCPaymentLibrary/src/ch/uzh/csg/paymentlib/util/Config.java
+++
@@ -1,9 +0,0 @@
-package ch.uzh.csg.paymentlib.util;
-
-//TODO: javadoc
-public class Config {
-	
-	public static final long SERVER_CALL_TIMEOUT = 3 * 1000; //in ms - server call
-	public static final long SERVER_RESPONSE_TIMEOUT = 4 * 000; //in ms - PaymentRequestHandler waiting for server response
-
-}
